1cf70a9578f70b09406c14ef18c6e4bd7b519898
blue-sky-artyn <bluesky.artyn@yahoo.com>
2020-12-27 18:33:28 -0500

    delete unnecessary items (2020/12/27)

--- BlueSkyPortal/BlueSkyPortal/Default.aspx.cs
+++ BlueSkyPortal/BlueSkyPortal/Default.aspx.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Cruder.Core;
 
-using bluesky.artyn;
 using System.Xml.Linq;
 
 
--- BlueSkyPortal/BlueSkyPortal/t1.aspx.vb
+++
@@ -1,5 +0,0 @@
-﻿
-Partial Class t1
-    Inherits System.Web.UI.Page
-
-End Class
